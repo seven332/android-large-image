471cc1b71a6b0a88d56c65833e005684d315d31b
seven332 <seven332@163.com>
2018-01-27 10:05:55 +0800

    Improve WrapperDrawable

--- large-image/src/main/java/com/hippo/android/largeimage/WrapperDrawable.java
+++ large-image/src/main/java/com/hippo/android/largeimage/WrapperDrawable.java
@@ -20,13 +20,18 @@ package com.hippo.android.largeimage;
  * Created by Hippo on 2018/1/25.
  */
 
+import android.content.res.ColorStateList;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.Outline;
 import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 
 /**
  * Drawable container with only one child element.
@@ -48,16 +53,10 @@ class WrapperDrawable extends Drawable implements Drawable.Callback {
       drawable.setCallback(this);
     }
 
+    this.drawable = drawable;
     onSetWrappedDrawable(this.drawable, drawable);
 
-    this.drawable = drawable;
-
     invalidateSelf();
-  }
-
-  @Nullable
-  public Drawable getDrawable() {
-    return drawable;
   }
 
   public void onSetWrappedDrawable(@Nullable Drawable oldDrawable, @Nullable Drawable newDrawable) {
@@ -66,21 +65,9 @@ class WrapperDrawable extends Drawable implements Drawable.Callback {
     }
   }
 
-  @Override
-  public int getIntrinsicWidth() {
-    return drawable != null ? drawable.getIntrinsicWidth() : 0;
-  }
-
-  @Override
-  public int getIntrinsicHeight() {
-    return drawable != null ? drawable.getIntrinsicHeight() : 0;
-  }
-
-  @Override
-  protected void onBoundsChange(Rect bounds) {
-    if (drawable != null) {
-      drawable.setBounds(bounds);
-    }
+  @Nullable
+  public Drawable getDrawable() {
+    return drawable;
   }
 
   @Override
@@ -91,10 +78,54 @@ class WrapperDrawable extends Drawable implements Drawable.Callback {
   }
 
   @Override
+  public boolean getPadding(@NonNull Rect padding) {
+    return drawable != null && drawable.getPadding(padding);
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override
+  public void setHotspot(float x, float y) {
+    if (drawable != null) {
+      drawable.setHotspot(x, y);
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override
+  public void setHotspotBounds(int left, int top, int right, int bottom) {
+    if (drawable != null) {
+      drawable.setHotspotBounds(left, top, right, bottom);
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.M)
+  @Override
+  public void getHotspotBounds(@NonNull Rect outRect) {
+    if (drawable != null) {
+      drawable.getHotspotBounds(outRect);
+    } else {
+      outRect.set(getBounds());
+    }
+  }
+
+  @Override
+  public boolean setVisible(boolean visible, boolean restart) {
+    final boolean superChanged = super.setVisible(visible, restart);
+    final boolean changed = drawable != null && drawable.setVisible(visible, restart);
+    return superChanged | changed;
+  }
+
+  @Override
   public void setAlpha(int alpha) {
     if (drawable != null) {
       drawable.setAlpha(alpha);
     }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+  @Override
+  public int getAlpha() {
+    return drawable != null ? drawable.getAlpha() : 255;
   }
 
   @Override
@@ -104,12 +135,79 @@ class WrapperDrawable extends Drawable implements Drawable.Callback {
     }
   }
 
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override
+  public void setTintList(@Nullable ColorStateList tint) {
+    if (drawable != null) {
+      drawable.setTintList(tint);
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override
+  public void setTintMode(@NonNull PorterDuff.Mode tintMode) {
+    if (drawable != null) {
+      drawable.setTintMode(tintMode);
+    }
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.M)
+  @Override
+  public boolean onLayoutDirectionChanged(int layoutDirection) {
+    return drawable != null && drawable.setLayoutDirection(layoutDirection);
+  }
+
   @Override
   public int getOpacity() {
+    return drawable != null ? drawable.getOpacity() : PixelFormat.TRANSPARENT;
+  }
+
+  @Override
+  public boolean isStateful() {
+    return drawable != null && drawable.isStateful();
+  }
+
+  @Override
+  protected boolean onStateChange(int[] state) {
+    if (drawable != null && drawable.isStateful()) {
+      final boolean changed = drawable.setState(state);
+      if (changed) {
+        onBoundsChange(getBounds());
+      }
+      return changed;
+    }
+    return false;
+  }
+
+  @Override
+  protected boolean onLevelChange(int level) {
+    return drawable != null && drawable.setLevel(level);
+  }
+
+  @Override
+  protected void onBoundsChange(Rect bounds) {
     if (drawable != null) {
-      return drawable.getOpacity();
+      drawable.setBounds(bounds);
+    }
+  }
+
+  @Override
+  public int getIntrinsicWidth() {
+    return drawable != null ? drawable.getIntrinsicWidth() : -1;
+  }
+
+  @Override
+  public int getIntrinsicHeight() {
+    return drawable != null ? drawable.getIntrinsicHeight() : -1;
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override
+  public void getOutline(@NonNull Outline outline) {
+    if (drawable != null) {
+      drawable.getOutline(outline);
     } else {
-      return PixelFormat.TRANSPARENT;
+      super.getOutline(outline);
     }
   }
 
